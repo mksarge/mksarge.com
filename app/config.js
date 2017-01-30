fb21327426034b014336c760a9fdac6c9fc0659d
Michael Sargent <michaelksarge@gmail.com>
2017-01-30 06:55:26 -0800

    Update homepage

--- app/config.js
+++ app/config.js
@@ -8,6 +8,23 @@ const config = {
     { path: '/', name: 'home' },
     { path: '/blog', name: 'blog' },
   ],
+  projects: [
+    {
+      name: 'ClassPortal',
+      text: 'Course management system',
+      url: 'https://ubc-310-classportal.firebaseapp.com',
+    },
+    {
+      name: 'React Starter App',
+      text: 'Static React frontend boilerplate',
+      url: 'https://react-starter-app.firebaseapp.com/',
+    },
+    {
+      name: 'Node-TypeScript-Restify-Mongoose',
+      text: 'Node.js REST API boilerplate',
+      url: 'https://github.com/mksarge/node-typescript-restify-mongoose',
+    },
+  ],
 };
 
 export default config;
--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Page from '../../components/Page';
+import css from './index.css';
+import posts from '../../posts';
+import config from '../../config';
+
+const renderPosts = () => posts.slice(0, 3).map((post) => (
+  <li key={post.url}>
+    <h5 className={css.link}>
+      <Link to={`/blog/${post.url}`}>{post.title}</Link>
+    </h5>
+  </li>));
+
+const renderProjects = () => config.projects.map((project) => (
+  <li key={project.url}>
+    <h5 className={css.link}>
+      <a href={project.url} target="_blank" rel="noopener noreferrer">
+        {project.name}
+      </a>
+    </h5>
+    <h6 className={css.subtext}>{project.text}</h6>
+  </li>));
 
 const HomePage = () => (
   <Page title="Home">
-    <p>Home</p>
+    <h1>Recent Posts</h1>
+    <ul>
+      {renderPosts()}
+      <Link to="/blog">→ View all posts</Link>
+    </ul>
+    <hr />
+    <h1>Projects</h1>
+    <ul>
+      {renderProjects()}
+      <a href={config.github} target="_blank" rel="noopener noreferrer">→ View on GitHub</a>
+    </ul>
   </Page>
 );
 
