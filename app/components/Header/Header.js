362f688726208822d13ca447a1d3172a402c1fd0
Michael Sargent <michaelksarge@gmail.com>
2017-01-30 07:10:59 -0800

    Add profile image to Header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,30 +1,26 @@
-import React, { PropTypes } from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
+import React from 'react';
 import css from './Header.css';
+import { getRootPath } from '../../utils';
 
-const Header = () => (
-  <div className={css.header} >
-    <div className={css.container}>
-      <div className={css.image}>
-        <img src="assets/profile.png" alt="" />
-      </div>
-      <div className={css.block}>
-        <p>
-          Hey, I&apos;m Michael. I&apos;m a software developer
+const Header = () => {
+  const path = getRootPath();
+  return path === 'home' && (
+    <div className={css.header} >
+      <div className={css.container}>
+        <div className={css.image}>
+          <img src="assets/profile.png" alt="" />
+        </div>
+        <div className={css.block}>
+          <p>
+            Hey, I&apos;m Michael. I&apos;m a software developer
             working at&nbsp;
-            <a href="https://www.ubc.ca/" target="_blank" rel="noopener noreferrer">UBC</a>
-          , currently building cool things with React.
+            <a href="https://www.ubc.ca/" target="_blank" rel="noopener noreferrer">UBC</a>.
           </p>
+        </div>
       </div>
-    </div>
-  </div>
-);
-
-Header.propTypes = {
-  text: PropTypes.string,
-};
-
-Header.defaultProps = {
-  text: '',
+    </div>);
 };
 
 export default Header;
