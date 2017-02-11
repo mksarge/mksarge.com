fe36c9cf22806e17fff97557784692c650e9ba28
Michael Sargent <michaelksarge@gmail.com>
2017-02-10 23:28:26 -0800

    Pass route to Navbar from parent

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import css from './Navbar.css';
 import config from '../../config';
-import { getRootPath } from '../../utils';
 
-const renderLinks = () => config.routes.map((link) => {
-  const currentPath = getRootPath();
-  return (<Link
-    key={link.name}
-    className={css.navblock}
-    to={link.path}
-    style={currentPath === link.name
-      ? { background: 'rgba(255, 255, 255, 0.2)' }
-      : {}}
-  > {link.name}
-  </Link >);
-});
-
-const Navbar = () => (
+const Navbar = (props) => (
   <div className={css.navbar}>
     <div className={css.container}>
-      {renderLinks()}
+      {config.routes.map((link) => (
+        <Link
+          key={link.name}
+          className={css.navblock}
+          to={link.path}
+          style={props.path === link.name
+            ? { background: 'rgba(255, 255, 255, 0.2)' }
+            : {}}
+        >{link.name}
+        </Link >))}
     </div>
   </div>
 );
+
+Navbar.propTypes = {
+  path: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  path: '',
+};
 
 export default Navbar;
