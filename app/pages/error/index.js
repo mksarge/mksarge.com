8f1f62461987b31b62a329f6462d480f5c95cb69
Michael Sargent <michaelksarge@gmail.com>
2017-02-10 23:40:24 -0800

    Fix error page back button

--- app/pages/error/index.js
+++ app/pages/error/index.js
@@ -1,21 +1,30 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
 import React from 'react';
 import { browserHistory, Link } from 'react-router';
 import Page from '../../components/Page';
 import css from './index.css';
+
+const goBack = (e) => {
+  e.preventDefault();
+  browserHistory.goBack();
+};
 
 const ErrorPage = () => (
   <Page title="Page Not Found">
     <div className={css.container}>
       <div className={css.status}>
         <h1>404</h1>
-        <p>Page Not Found</p>
+        <h2>Page Not Found</h2>
       </div>
       <p>
         The page you&apos;re looking for does not exist.
-          <br />
-        <a href="/" onClick={() => browserHistory.goBack()}>Go back</a>
-        , or head to the &nbsp;<Link to="/">home page</Link>.
-        </p>
+      </p>
+      <div className={css.fakeLink} onClick={goBack}>Go back</div>
+      <p style={{ display: 'inline-block' }}>
+        , or head to the&nbsp;
+        <Link to="/">home page</Link>.
+      </p>
     </div>
   </Page>
 );
