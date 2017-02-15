cb063217f84acf8455ba0c98ef793c0d27bc1cec
Michael Sargent <michaelksarge@gmail.com>
2017-02-15 14:41:48 -0800

    Update Layout

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -6,8 +6,13 @@ const Footer = () => (
   <footer className={css.footer}>
     <div className={css.container}>
       <p className={css.text}>
-        &copy; 2017 Michael Sargent&nbsp;
-        <a href={config.source}>(source)</a>
+        &copy; 2017 Michael Sargent
+        <br />
+        <a href={config.github}>github</a>
+        &nbsp;&middot;&nbsp;
+        <a href={config.twitter}>twitter</a>
+        &nbsp;&middot;&nbsp;
+        <a href={`mailto:${config.email}`}>email</a>
       </p>
     </div>
   </footer>
--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -6,16 +6,18 @@ import config from '../../config';
 const Navbar = (props) => (
   <div className={css.navbar}>
     <div className={css.container}>
-      {config.routes.map((link) => (
-        <Link
-          key={link.name}
-          className={css.navblock}
-          to={link.path}
-          style={props.path === link.name
-            ? { background: 'rgba(255, 255, 255, 0.2)' }
-            : {}}
-        >{link.name}
-        </Link >))}
+      <p>
+        {config.routes.map((link) => (
+          <Link
+            key={link.name}
+            className={css.navblock}
+            to={link.path}
+            style={props.path === link.name
+              ? { background: 'rgba(255, 255, 255, 0.2)' }
+              : {}}
+          >{link.name}
+          </Link >))}
+      </p>
     </div>
   </div>
 );
