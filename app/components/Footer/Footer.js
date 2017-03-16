d3640ce0c7a1c863971fdea933ee5460bcffea2d
Michael Sargent <michaelksarge@gmail.com>
2017-03-15 23:21:24 -0700

    Move social media links to Header component

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import css from './Footer.css';
-import config from '../../config';
-
-const links = [
-  {
-    name: 'github',
-    location: config.github,
-  },
-  {
-    name: 'twitter',
-    location: config.twitter,
-  },
-  {
-    name: 'email',
-    location: config.email,
-  },
-];
 
 const Footer = () => (
   <footer className={css.footer}>
     <div className={css.container}>
-      <p className={css.text}>
-        &copy; 2017 Michael Sargent
-        <br />
-        {links.map((link) => (
-          <a className={css.link} href={link.location}>{link.name}</a>
-        ))}
-      </p>
+      <p className={css.text}>&copy; 2017 Michael Sargent</p>
     </div>
   </footer>
 );
--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -3,17 +3,47 @@ import TextCarousel from './TextCarousel';
 import css from './Header.css';
 import config from '../../config';
 
+import profileIcon from './icons/profile.png';
+import githubIcon from './icons/github.svg';
+import twitterIcon from './icons/twitter.svg';
+import emailIcon from './icons/email.svg';
+
+const links = [
+  {
+    icon: githubIcon,
+    link: config.github,
+    alt: 'GitHub',
+  },
+  {
+    icon: twitterIcon,
+    link: config.twitter,
+    alt: 'Twitter',
+  },
+  {
+    icon: emailIcon,
+    link: config.email,
+    alt: 'Email',
+  },
+];
+
 const Header = (props) => (props.path === 'home') && (
   <div className={css.header} >
     <div className={css.container}>
-      <img className={css.image} src="/assets/profile.png" alt="" />
+      <img className={css.image} src={profileIcon} alt="" />
       <h4 className={css.text}>
         Hey, I&apos;m Michael. I&apos;m a software developer
         working at&nbsp;
-        <a href="https://www.ubc.ca/" target="_blank" rel="noopener noreferrer">UBC</a>
+        <a href="https://www.ubc.ca/">UBC</a>
         , currently...&nbsp;
       </h4>
       <TextCarousel className={css.carousel} input={config.headerText} />
+      <div className={css.iconContainer}>
+        {links.map((val, i) => (
+          <a key={i.toString()} href={val.link}>
+            <img className={css.icon} src={val.icon} alt={val.alt} />
+          </a>
+        ))}
+      </div>
     </div>
   </div>);
 
