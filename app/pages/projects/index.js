fb66fc2c63e7662b01e01aecdf46d14f3ff4b61d
Michael Sargent <michaelksarge@gmail.com>
2017-03-15 23:50:55 -0700

    Update projects page to use Paragraph component

--- app/pages/projects/index.js
+++ app/pages/projects/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Page from '../../components/Page';
-import css from '../home/index.css';
+import Paragraph from '../../components/Paragraph';
+import css from './index.css';
 import { config, projects } from '../../../config';
 
-const renderProjects = projects.map((project) => (
-  <li key={project.url}>
-    <h5 className={css.link}><a href={project.url} target="_blank" rel="noopener noreferrer">{project.name}</a></h5>
-    <p className={css.description}>{project.text}</p>
-  </li>));
-
-const BlogPage = () => (
-  <Page title="Blog">
+const PostsIndex = () => (
+  <Page title="Posts">
     <h2 className={css.heading}>Projects</h2>
-    <a href={config.github} target="_blank" rel="noopener noreferrer">» View All</a>
-    <ul>
-      {renderProjects}
-    </ul>
+    <a href={config.github}>» View on GitHub</a>
+    {projects.map((project, i) => (
+      <Paragraph
+        key={i.toString()}
+        icon={project.icon}
+        title={project.name}
+        text={project.text}
+      />
+    ))}
   </Page>
 );
 
-export default BlogPage;
+export default PostsIndex;
