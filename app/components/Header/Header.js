46a9b7290c99d06eb55ac0d66722d9a1003abb90
Michael Sargent <michaelksarge@gmail.com>
2017-04-26 21:23:06 -0700

    Import PropTypes from prop-types package

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import TextCarousel from './TextCarousel';
 import css from './Header.css';
 import config from '../../../config/config.json';
--- app/components/Paragraph/Paragraph.js
+++ app/components/Paragraph/Paragraph.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import css from './Paragraph.css';
 
 const Paragraph = ({ icon, title, text }) => (
