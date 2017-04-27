8c7f0067642df77f505edcf13537878bb5c5445f
Michael Sargent <michaelksarge@gmail.com>
2017-04-26 21:26:04 -0700

    Update footer text

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import css from './Footer.css';
-import config from '../../../config/config.json';
 
 const Footer = () => (
   <footer className={css.footer}>
     <div className={css.container}>
-      <p className={css.text}>
-        &copy; 2017 Michael Sargent&nbsp;
-        <a href={config.github}>➡</a>
-      </p>
+      <p className={css.text}>&copy; 2017 Michael Sargent</p>
     </div>
   </footer>
 );
