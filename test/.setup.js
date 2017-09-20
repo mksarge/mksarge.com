8f66000d1541a51f96587f39bb089ece1a66a089
Michael Sargent <michaelsar@trulia.com>
2017-09-19 21:12:47 -0700

    Update package.json

--- test/.setup.js
+++ test/.setup.js
@@ -1,23 +1,21 @@
+// Enzyme/Mocha setup: https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
+
 require('babel-register')();
-require('ignore-styles').default(undefined, () => ({
-  styleName: 'fake_class_name'
-}));
+var JSDOM = require('jsdom').JSDOM;
 
-var jsdom = require('jsdom').jsdom;
+var jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+var window = jsdom.window;
 
-var exposedProperties = ['window', 'navigator', 'document'];
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter((prop) => typeof target[prop] === 'undefined')
+    .map((prop) => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
 
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
-});
+global.window = window;
+global.document = window.document;
+global.HTMLElement = window.HTMLElement;
+global.navigator = { userAgent: 'node.js' };
 
-global.navigator = {
-  userAgent: 'node.js'
-};
-
-documentRef = document;
+copyProps(window, global);
